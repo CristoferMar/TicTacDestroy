bf52697d9dea0f62ba48e08b5b9908d823901ad0
CristoferMar <cristoferm@gmail.com>
2021-12-06 14:38:59 -0800

    adding parse route to project | in progress

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import parseRoute from './lib/parse-route';
 import Home from './pages/home';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuthorizing: window.localStorage.getItem('tic-tac-destroy') === null,
+      route: parseRoute(window.location.hash)
+
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      this.setState({ route: parseRoute(window.location.hash) });
+    });
+  }
+
+  renderPage() {
+    const { route } = this.state;
+    console.log(route);
+    return (<div>{`we are currently on ${route}`}</div>);
+  }
+
   render() {
-    return <Home />;
+    return (
+    <>
+      this.renderPage(this.state.route)
+    </>
+    );
   }
 }
