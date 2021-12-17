6f800538f8e023df48b61e1fba8ff3ef5f9e5e1f
Mike Ayala <ayala.s.mike@gmail.com>
2021-12-17 15:45:06 -0800

    used a ternary for the landing page to not show the small logo but still show up for everything else.

--- client/pages/nav-bar.jsx
+++ client/pages/nav-bar.jsx
@@ -9,8 +9,10 @@ export default class NavBar extends React.Component {
   }
 
   render() {
-    const single = ['Lobby', 'Results'].includes(this.props.path);
-    const signOn = ['Sign-Up', 'Sign-In'].includes(this.props.path);
+    const path = this.props.path;
+    const single = ['Lobby', 'Results'].includes(path);
+    const signOn = ['Sign-Up', 'Sign-In', 'Landing-Page'].includes(path);
+    const landing = path === 'Landing-Page';
     return (
       <div className="full-width height7-min40 flex-between padding-5 align-center">
 
@@ -26,14 +28,17 @@ export default class NavBar extends React.Component {
         {
         signOn &&
         <>
-          <img src="/images/small-logo.svg" alt="TicTacDestroy" className="height-90" />
+          {
+            landing
+              ? <div></div>
+              : <img src="/images/small-logo.svg" alt="TicTacDestroy" className="height-90" />
+          }
           <div className="flex-between">
             <span className="nav-buttons"><a href="#" className="nav-buttons-link sign-in-content"></a></span>
             <span className="nav-buttons wide-button"><a href="#" className="nav-buttons-link play-as-guest"></a></span>
           </div>
         </>
         }
-
       </div>
     );
   }
