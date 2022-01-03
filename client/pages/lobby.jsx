720e94eacc5139ab0d3ad25a5d9a98af0bb19e17
Daniel Hong <dan8492@gmail.com>
2022-01-03 10:23:49 -0800

    adjusted the border and made it responsive. also worked on the 'game lobby' title and also made it responsive.

--- client/pages/lobby.jsx
+++ client/pages/lobby.jsx
@@ -11,7 +11,11 @@ export default class Lobby extends React.Component {
   render() {
     return (
       <div className="full-width height-min-nav center-all">
-       <div className="border"><h1>hello</h1></div>
+       <div className="border flex justify-center">
+         <div className="custom-heading-1">
+           <span className="green">Game</span> <span className="pink">Lobby</span>
+         </div>
+        </div>
       </div>
     );
   }
