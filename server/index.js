9466321c6be3ace40446fbdd73511a5093830a7e
CristoferMar <cristoferm@gmail.com>
2022-01-06 11:50:57 -0800

    added middlewear for errors and authorization

--- server/index.js
+++ server/index.js
@@ -1,11 +1,32 @@
 require('dotenv/config');
+const pg = require('pg');
 const express = require('express');
+const ClientError = require('./client-error');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+const authorizationMiddleware = require('./authorization-middleware');
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 const app = express();
 
+const jsonMiddleware = express.json();
+
+app.use(jsonMiddleware);
+
 app.use(staticMiddleware);
+
+// login code
+
+app.use(authorizationMiddleware);
+// code post-authorization
 
 app.use(errorMiddleware);
 
